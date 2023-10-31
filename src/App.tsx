617722f9dff81d2d48064422bd8abfcd1d6c0956
Rogerio <rogerio.santana@df.docente.senai.br>
2023-10-31 09:58:09 -0300

    Subindo projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+interface InterfaceTarefa {
+  name: string,
+  id: number
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [tarefas, setTarefas] = useState<InterfaceTarefa[]>([])
 
+  const [nomeTarefa, setNomeTarefa] = useState<string>('')
+
+  const salvaTarefa = useCallback(() => {
+    setTarefas(value => [...tarefas,
+    {
+      name: nomeTarefa,
+      id: Math.random() * 100
+    }
+  ])
+
+    console.log(tarefas)
+  }, [nomeTarefa])
+
+  const removeTarefa = useCallback((id:number) => {
+    setTarefas(value => value.filter(v => v.id !==id))
+  },[])
+
+  useEffect(() => {
+    console.log(tarefas)
+  }, [tarefas])
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h2>Tarefas salvas</h2>
+        <ul>
+
+          {tarefas.map(tarefa => (
+            <li onClick={() => {removeTarefa(tarefa.id)}} key={tarefa.id}>{tarefa.name}</li>
+          ))}
+        </ul>
+
+        <input type="text" onChange={(e) => setNomeTarefa(e.target.value)} name="tarefa" placeholder='Insira a sua tarefa' />
+        <button onClick={salvaTarefa}>
+          Salvar Tarefa
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
